99319d7ed315eff4845e49d7490607972de86e37
fabiano-debugador <fabiano_the_beast@hotmail.com>
2021-12-27 13:48:05 -0300

    create controller and routes of the products

--- src/shared/typeorm/migrations/1637019088858-CreateProfile.ts
+++ src/shared/typeorm/migrations/1637019088858-CreateProfile.ts
@@ -33,7 +33,7 @@ export class CreateProfile1637019088858 implements MigrationInterface {
             default: 'now()',
           },
           {
-            name: 'update_at',
+            name: 'updated_at',
             type: 'timestamp',
             default: 'now()',
           },
