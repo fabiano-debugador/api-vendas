06897f462364a1a63ac4c0a8240f77ef5e407817
fabiano-debugador <fabiano_the_beast@hotmail.com>
2022-03-01 12:55:37 -0300

    create orders

--- src/modules/orders/typeorm/entities/Order.ts
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -1,12 +1,15 @@
-import Customer from '@modules/customers/typeorm/entities/Customer';
 import {
   CreateDateColumn,
   Entity,
   JoinColumn,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+
+import Customer from '@modules/customers/typeorm/entities/Customer';
+import OrdersProducts from './OrdersProducts';
 
 @Entity('orders')
 class Order {
@@ -16,6 +19,11 @@ class Order {
   @ManyToOne(() => Customer)
   @JoinColumn({ name: 'customer_id' })
   customer: Customer;
+
+  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
+    cascade: true,
+  })
+  order_products: OrdersProducts[];
 
   @CreateDateColumn()
   created_at: Date;
